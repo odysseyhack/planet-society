f66f255ce4be14a1106d0405bdab0b7c482027c0
phob0s <git@phob0s.pl>
2019-04-12 19:29:30 +0200

    requester refactoring

--- protocol/cmd/requester/main.go
+++ protocol/cmd/requester/main.go
@@ -7,12 +7,13 @@ import (
 	"net/url"
 	"os"
 
+	"github.com/odysseyhack/planet-society/protocol/utils"
+
 	"github.com/gorilla/websocket"
 	"github.com/odysseyhack/planet-society/protocol/cryptography"
 	"github.com/odysseyhack/planet-society/protocol/models"
 	"github.com/odysseyhack/planet-society/protocol/protocol"
 	"github.com/odysseyhack/planet-society/protocol/transport"
-	"github.com/odysseyhack/planet-society/protocol/utils"
 )
 
 const query = `
@@ -31,31 +32,25 @@ query {
 
 const (
 	responderAddress = ":15000"
+	requester        = "John Smith"
+)
+
+var (
+	keychain           *cryptography.Keychain
+	transactionID      *cryptography.Key32
+	responderPublicKey *cryptography.Key32
 )
 
 func main() {
-	fmt.Println("-> generating identity")
-	keychain, err := cryptography.OneShotKeychain()
-	if err != nil {
-		fmt.Println("-> failed to generate identity")
-		os.Exit(1)
+	fmt.Println("-> generating transaction context")
+	if err := createContext(); err != nil {
+		fail("-> generating transaction context failed:", err)
 	}
-
-	transactionID := cryptography.RandomKey32()
-	fmt.Println("-> using unique transaction ID:", transactionID.String())
 	fmt.Println("-> connecting to the responder")
-
-	responderKey, err := utils.ReadKeyFromDir()
-	if err != nil {
-		fmt.Println("-> failed to get responder public key")
-		os.Exit(1)
-	}
-	fmt.Println("-> using responder public key:", responderKey.String())
 
 	conn, err := connectToResponder()
 	if err != nil {
-		fmt.Println("-> failed to connect to responder")
-		os.Exit(1)
+		fail("-> failed to connect to responder")
 	}
 
 	defer func() {
@@ -64,66 +59,125 @@ func main() {
 			fmt.Println("-> failed to close connection to the responder")
 		}
 	}()
-	fmt.Println("-> connected to the responder")
-	fmt.Println("-> sending pre transaction request")
 
-	sendMsg := func(topic cryptography.Key32, payload interface{}) error {
-		var buffer bytes.Buffer
-
-		if err := gob.NewEncoder(&buffer).Encode(payload); err != nil {
-			return err
-		}
-
-		msg := &protocol.Message{
-			Header: protocol.Header{
-				Source:      keychain.MainPublicKey,
-				Destination: responderKey,
-				Topic:       topic,
-			},
-			Body: protocol.Body{
-				Payload: buffer.Bytes(),
-			},
-		}
-		return conn.Write(msg)
+	if err := preTransact(conn); err != nil {
+		fmt.Println("-> pre transaction failed:", err)
+		os.Exit(1)
 	}
 
-	err = sendMsg(
+	if err := transact(conn); err != nil {
+		fmt.Println("-> transaction failed:", err)
+		os.Exit(1)
+	}
+}
+
+func connectToResponder() (*Transport, error) {
+	u := url.URL{Scheme: "ws", Host: responderAddress, Path: "/"}
+	conn, _, err := websocket.DefaultDialer.Dial(u.String(), nil)
+	if err != nil {
+		return nil, err
+	}
+	fmt.Println("-> connected to the responder")
+	return &Transport{&transport.Conn{Conn: conn}}, err
+}
+
+func createContext() error {
+	k, err := cryptography.OneShotKeychain()
+	if err != nil {
+		return err
+	}
+
+	responderKey, err := utils.ReadKeyFromDir()
+	if err != nil {
+		return err
+	}
+
+	tID := cryptography.RandomKey32()
+	keychain = k
+	transactionID = &tID
+	responderPublicKey = &responderKey
+
+	fmt.Println("-> using responder public key:", responderKey.String())
+	fmt.Println("-> using unique transaction ID:", transactionID.String())
+
+	return nil
+}
+
+func fail(a ...interface{}) {
+	fmt.Println(a)
+	os.Exit(1)
+}
+
+type Transport struct {
+	conn *transport.Conn
+}
+
+func (t *Transport) SendMessage(topic cryptography.Key32, payload interface{}) error {
+	var buffer bytes.Buffer
+
+	if err := gob.NewEncoder(&buffer).Encode(payload); err != nil {
+		return err
+	}
+
+	msg := &protocol.Message{
+		Header: protocol.Header{
+			Source:      keychain.MainPublicKey,
+			Destination: *responderPublicKey,
+			Topic:       topic,
+		},
+		Body: protocol.Body{
+			Payload: buffer.Bytes(),
+		},
+	}
+	return t.conn.Write(msg)
+}
+
+func (t *Transport) Close() error {
+	return t.conn.Close()
+}
+
+func (t *Transport) Read() (*protocol.Message, error) {
+	return t.conn.Read()
+}
+
+func preTransact(conn *Transport) error {
+	fmt.Println("-> sending pre transaction request")
+
+	if err := conn.SendMessage(
 		protocol.TopicPreTransactionRequest,
 		&models.PreTransactionRequest{
-			TransactionID:      models.Key32{Key: transactionID},
+			TransactionID:      models.Key32{Key: *transactionID},
 			SignaturePublicKey: models.Key32{Key: keychain.SignaturePublicKey},
 			MainPublicKey:      models.Key32{Key: keychain.MainPublicKey},
 			Requester:          "John Smith",
 		},
-	)
-	if err != nil {
-		fmt.Println("-> sending pre transaction failed:", err)
-		os.Exit(1)
+	); err != nil {
+		return err
 	}
 
 	msg, err := conn.Read()
 	if err != nil {
-		fmt.Println("-> read failed:", err)
-		os.Exit(1)
+		return err
 	}
 
 	var preTransactionReply models.PreTransactionReply
 	if err := gob.NewDecoder(bytes.NewBuffer(msg.Body.Payload)).Decode(&preTransactionReply); err != nil {
-		fmt.Println("-> pre transaction reply: invalid payload:", err)
-		os.Exit(1)
+		return fmt.Errorf("pre transaction reply invalid payload: %s", err)
 	}
 
 	if !preTransactionReply.Success {
-		fmt.Println("-> pre transaction was not successful")
-		os.Exit(1)
+		return fmt.Errorf("pre transaction was not successful")
 	}
-
 	fmt.Println("-> received positive pre transaction response")
+	return nil
+}
+
+func transact(conn *Transport) error {
 	fmt.Println("-> sending transaction request")
-	err = sendMsg(
+	err := conn.SendMessage(
 		protocol.TopicTransactionRequest,
 		&models.TransactionRequest{
-			TransactionID: models.Key32{Key: transactionID},
+			TransactionID: models.Key32{Key: *transactionID},
 			Query:         query,
 			Reason:        "please provide me following details to finalize shipment",
 			LawApplying:   "European Union",
@@ -135,7 +189,7 @@ func main() {
 	}
 
 	fmt.Println("-> Waiting for transaction reply")
-	msg, err = conn.Read()
+	msg, err := conn.Read()
 	if err != nil {
 		fmt.Println("-> Read failed", err)
 		os.Exit(1)
@@ -160,13 +214,5 @@ func main() {
 
 	fmt.Println("-> received positive transaction response")
 	fmt.Println("Transaction content:", *transactionReply.Content)
-}
-
-func connectToResponder() (*transport.Conn, error) {
-	u := url.URL{Scheme: "ws", Host: responderAddress, Path: "/"}
-	conn, _, err := websocket.DefaultDialer.Dial(u.String(), nil)
-	if err != nil {
-		return nil, err
-	}
-	return &transport.Conn{Conn: conn}, err
+	return nil
 }
