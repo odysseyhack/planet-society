fbd7010891c617671fc48e94c62e35f73d1b232a
Corné Driesprong <cdriesprong@gmail.com>
2019-04-12 16:55:40 +0200

    added support for different cell types in TransactionOverviewViewController

--- Permission-Hub-iOS/Permission Hub/UI/Transaction Flow/TransactionOverviewViewController.swift
+++ Permission-Hub-iOS/Permission Hub/UI/Transaction Flow/TransactionOverviewViewController.swift
@@ -8,18 +8,55 @@
 
 import UIKit
 
+enum TransactionVerificationStatus {
+    case verified, unverified
+
+    var color: UIColor {
+        switch self {
+        case .verified:
+            return PHColors.topaz
+        case .unverified:
+            return PHColors.red
+        }
+    }
+}
+
+enum TransactionOverviewViewCellType {
+    case notification(
+        type: TransactionNotificationType,
+        text: String)
+    case description(text: String)
+    case transactionItem(item: TransactionItem)
+}
+
 final class TransactionOverviewViewController: UITableViewController {
 
     // MARK: - Private properties
 
     private let transaction: TransactionNotification
 
+    private var cells: [TransactionOverviewViewCellType] {
+
+        var cells = [TransactionOverviewViewCellType]()
+
+        cells.append(.notification(
+            type: .warning,
+            text: "Permission warning!"))
+        cells.append(.description(text: transaction.reason))
+
+        transaction.items.forEach {
+            cells.append(.transactionItem(item: $0))
+        }
+
+        return cells
+    }
+
     // MARK: - Initialization
 
     init(transaction: TransactionNotification) {
         self.transaction = transaction
 
-        super.init(nibName: nil, bundle: nil)
+        super.init(style: .plain)
     }
 
     required init?(coder aDecoder: NSCoder) {
@@ -42,6 +79,14 @@ final class TransactionOverviewViewController: UITableViewController {
             TransactionTableViewCell.self,
             forCellReuseIdentifier: String(describing: TransactionTableViewCell.self))
 
+        tableView.register(
+            UITableViewCell.self,
+            forCellReuseIdentifier: String(describing: UITableViewCell.self))
+
+        tableView.register(
+            TransactionNotificationTableViewCell.self,
+            forCellReuseIdentifier: String(describing: TransactionNotificationTableViewCell.self))
+
         tableView.rowHeight = UITableView.automaticDimension
         tableView.estimatedRowHeight = 66
         tableView.allowsSelection = false
@@ -50,15 +95,46 @@ final class TransactionOverviewViewController: UITableViewController {
     // MARK: - UITableViewDataSource
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return items.count
+        return cells.count
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
 
-        let cell = tableView.dequeueReusableCell(
-            withIdentifier: String(describing: TransactionTableViewCell.self),
-            for: indexPath)
+        switch cells[indexPath.row] {
+        case .notification(let type, let text):
 
-        return cell
+            let cell = tableView.dequeueReusableCell(
+                withIdentifier: String(describing: TransactionNotificationTableViewCell.self),
+                for: indexPath) as! TransactionNotificationTableViewCell
+
+            cell.configure(withType: type, andText: text)
+
+            return cell
+
+        case .description(let text):
+
+            let cell = tableView.dequeueReusableCell(
+                withIdentifier: String(describing: UITableViewCell.self),
+                for: indexPath)
+
+            cell.textLabel?.font = PHFonts.regular()
+            cell.textLabel?.text = text
+
+            return cell
+
+        case .transactionItem(let item):
+
+            let cell = tableView.dequeueReusableCell(
+                withIdentifier: String(describing: TransactionTableViewCell.self),
+                for: indexPath) as! TransactionTableViewCell
+
+            let viewModel = TransactionTableViewCellViewModel(
+                image: UIImage(),
+                title: item.item,
+                subtitle: (item.fields as NSArray).componentsJoined(by: ", "))
+            cell.configure(withViewModel: viewModel)
+
+            return cell
+        }
     }
 }
