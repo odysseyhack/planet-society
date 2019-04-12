10b67b21d1b2cdc900800f6950f1ba77ca398182
phob0s <git@phob0s.pl>
2019-04-12 15:49:42 +0200

    responder basic usage

--- protocol/cmd/responder/main.go
+++ protocol/cmd/responder/main.go
@@ -1,12 +1,18 @@
 package main
 
 import (
+	"context"
 	"io/ioutil"
+	"net/http"
 	"os"
 	"path/filepath"
 
+	"github.com/99designs/gqlgen/handler"
+	"github.com/go-chi/chi"
 	"github.com/odysseyhack/planet-society/protocol/cryptography"
 	"github.com/odysseyhack/planet-society/protocol/database"
+	"github.com/odysseyhack/planet-society/protocol/protocol"
+	"github.com/odysseyhack/planet-society/protocol/transport"
 	"github.com/odysseyhack/planet-society/protocol/utils"
 	log "github.com/sirupsen/logrus"
 )
@@ -72,5 +78,37 @@ func main() {
 		log.Fatalln("failed to fill db with data:", err)
 	}
 
-	select {}
+	router := chi.NewRouter()
+	router.Use(Middleware(db))
+	resolver := protocol.NewResolver(db)
+	router.Handle("/", handler.Playground("GraphQL playground", "/query"))
+	router.Handle("/query", handler.GraphQL(protocol.NewExecutableSchema(protocol.Config{Resolvers: resolver})))
+	go func() {
+		log.Warningln(http.ListenAndServe(":8088", router))
+	}()
+
+	proto := protocol.NewProtocol(nil)
+	go proto.Loop()
+
+	ws := transport.NewWebsocket(proto.Connections)
+	log.Infoln("starting listener at: :15000")
+	if err := ws.Listen(":15000"); err != nil {
+		log.Warningln("transport listener failed: %s", err)
+	}
+}
+
+func Middleware(db *database.Database) func(http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			// generate random key
+			key := cryptography.RandomKey32()
+
+			// put it in context
+			ctx := context.WithValue(r.Context(), "transaction-key", key.String())
+			// and call the next with our new context
+			r = r.WithContext(ctx)
+
+			next.ServeHTTP(w, r)
+		})
+	}
 }
