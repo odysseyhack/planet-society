a8ded6b8cc20b2989e332c58c7a98d677bde6cd7
Corné Driesprong <cdriesprong@gmail.com>
2019-04-12 15:00:57 +0200

    created PHFonts object

---
+++ Permission-Hub-iOS/Permission Hub/UI/PHFonts.swift
@@ -0,0 +1,13 @@
+//
+//  PHFonts.swift
+//  Permission Hub
+//
+//  Created by Corné on 12/04/2019.
+//  Copyright © 2019 Planet. All rights reserved.
+//
+
+import UIKit
+
+enum PHFonts {
+    static let regular = UIFont(name: "Helvetica", size: 13)
+}
