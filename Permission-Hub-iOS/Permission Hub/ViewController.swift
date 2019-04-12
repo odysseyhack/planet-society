37f790deb41b7df7f3236ba8c9d965c779d31a49
Corné Driesprong <cdriesprong@gmail.com>
2019-04-12 12:46:59 +0200

    created xcode project

---
+++ Permission-Hub-iOS/Permission Hub/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Permission Hub
+//
+//  Created by Corné on 12/04/2019.
+//  Copyright © 2019 Planet. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
