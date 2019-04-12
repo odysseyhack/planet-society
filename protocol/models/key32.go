9b0a76b79079abc81a8d33a43c53ae521b1d8239
phob0s <git@phob0s.pl>
2019-04-12 13:32:16 +0200

    models: add key32 type

---
+++ protocol/models/key32.go
@@ -0,0 +1,32 @@
+package models
+
+import (
+	"fmt"
+	"io"
+	"strconv"
+
+	"github.com/odysseyhack/planet-society/protocol/cryptography"
+)
+
+type Key32 struct {
+	Key cryptography.Key32
+}
+
+func (k Key32) MarshalGQL(w io.Writer) {
+	data := strconv.Quote(k.Key.String())
+	_, _ = w.Write([]byte(data))
+}
+
+func (k *Key32) UnmarshalGQL(v interface{}) error {
+	switch v := v.(type) {
+	case string:
+		key, err := cryptography.Key32FromString(v)
+		if err != nil {
+			return err
+		}
+		k.Key = key
+		return nil
+	default:
+		return fmt.Errorf("%T is invalid", v)
+	}
+}
