0b4a7ffac7f4135a1d7f2c3391ad8ed3293a7653
phob0s <git@phob0s.pl>
2019-04-12 21:06:49 +0200

    refactoring of responder main function

--- protocol/cmd/responder/main.go
+++ protocol/cmd/responder/main.go
@@ -21,6 +21,10 @@ const (
 	dbFile = "hackaton_db.db"
 )
 
+var (
+	keychain *cryptography.Keychain
+)
+
 func main() {
 	utils.ConfigureLogger()
 	log.Infoln("responder application")
@@ -39,13 +43,9 @@ func main() {
 		}
 	}()
 
-	log.Infoln("generating one shot keychain")
-	keychain, err := cryptography.OneShotKeychain()
-	if err != nil {
+	if err := createKeychain(); err != nil {
 		log.Fatalln("failed to generate keychain:", err)
 	}
-	log.Infoln("keychain main public key:", keychain.MainPublicKey.String())
-	log.Infoln("keychain signature key:  ", keychain.SignaturePublicKey.String())
 
 	if err := utils.WriteKeyToDir(keychain.MainPublicKey); err != nil {
 		log.Fatalln("failed to write public key:", err)
@@ -58,11 +58,9 @@ func main() {
 		}
 	}()
 
-	filePath := filepath.Join(dir, dbFile)
-	log.Infoln("using database:", filePath)
-	db, err := database.LoadDatabase(filePath, keychain)
+	db, err := createDatabase(dir)
 	if err != nil {
-		log.Fatalln("failed to load database:", err)
+		log.Fatalln("failed to create database:", err)
 	}
 
 	defer func() {
@@ -72,12 +70,12 @@ func main() {
 		}
 	}()
 
-	log.Infoln("filling database with items")
-	generator := newGenerator()
-	if err := generator.generate(db); err != nil {
-		log.Fatalln("failed to fill db with data:", err)
+	if err := serve(db); err != nil {
+		log.Warningf("%s", err)
 	}
+}
 
+func serve(db *database.Database) error {
 	router := chi.NewRouter()
 	router.Use(Middleware(db))
 	resolver := protocol.NewResolver(db)
@@ -92,9 +90,7 @@ func main() {
 
 	ws := transport.NewWebsocket(proto.Connections)
 	log.Infoln("starting listener at: :15000")
-	if err := ws.Listen(":15000"); err != nil {
-		log.Warningln("transport listener failed: %s", err)
-	}
+	return ws.Listen(":15000")
 }
 
 func Middleware(db *database.Database) func(http.Handler) http.Handler {
@@ -111,4 +107,32 @@ func Middleware(db *database.Database) func(http.Handler) http.Handler {
 			next.ServeHTTP(w, r)
 		})
 	}
+}
+
+func createKeychain() (err error) {
+	log.Infoln("generating one shot keychain")
+	keychain, err := cryptography.OneShotKeychain()
+	if err != nil {
+		return err
+	}
+	log.Infoln("keychain main public key:", keychain.MainPublicKey.String())
+	log.Infoln("keychain signature key:  ", keychain.SignaturePublicKey.String())
+	return nil
+}
+
+func createDatabase(dir string) (*database.Database, error) {
+	filePath := filepath.Join(dir, dbFile)
+	log.Infoln("using database:", filePath)
+	db, err := database.LoadDatabase(filePath, keychain)
+	if err != nil {
+		return nil, err
+	}
+
+	log.Infoln("filling database with items")
+	generator := newGenerator()
+	if err := generator.generate(db); err != nil {
+		return nil, err
+	}
+
+	return db, nil
 }
