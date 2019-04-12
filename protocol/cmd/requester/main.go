c0df94a5c3a9796581495cf93d8fc0fe95839788
phob0s <git@phob0s.pl>
2019-04-12 13:58:42 +0200

    responder binary draft

--- protocol/cmd/requester/main.go
+++ protocol/cmd/requester/main.go
@@ -1,7 +1,37 @@
 package main
 
-import "github.com/odysseyhack/planet-society/protocol/utils"
+import (
+	"fmt"
+	"github.com/odysseyhack/planet-society/protocol/cryptography"
+	"github.com/odysseyhack/planet-society/protocol/utils"
+)
+
+const query = `
+query {
+  personalDetails {
+    name
+    surname
+  }
+  address {
+    country
+    city
+    street
+  }
+}
+`
+
+const (
+	responderAddress = ":15000"
+)
 
 func main() {
 	utils.ConfigureLogger()
+
+
+	transactionID := cryptography.RandomKey32()
+	fmt.Println("-> using unique transaction ID:", transactionID.String())
+	fmt.Println("-> sending pre transaction request")
+	fmt.Println("-> received positive pre transaction response")
+	fmt.Println("-> sending transaction request")
+	fmt.Println("-> received positive transaction response")
 }
