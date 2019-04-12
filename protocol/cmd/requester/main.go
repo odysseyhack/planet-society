198dde63ef3f922a0b5476663f7c3fe26ebabc43
phob0s <git@phob0s.pl>
2019-04-12 14:48:30 +0200

    requester and responder public key exchange

--- protocol/cmd/requester/main.go
+++ protocol/cmd/requester/main.go
@@ -2,7 +2,12 @@ package main
 
 import (
 	"fmt"
+	"net/url"
+	"os"
+
+	"github.com/gorilla/websocket"
 	"github.com/odysseyhack/planet-society/protocol/cryptography"
+	"github.com/odysseyhack/planet-society/protocol/transport"
 	"github.com/odysseyhack/planet-society/protocol/utils"
 )
 
@@ -25,13 +30,42 @@ const (
 )
 
 func main() {
-	utils.ConfigureLogger()
-
-
 	transactionID := cryptography.RandomKey32()
 	fmt.Println("-> using unique transaction ID:", transactionID.String())
+	fmt.Println("-> connecting to the responder")
+
+	responderKey, err := utils.ReadKeyFromDir()
+	if err != nil {
+		fmt.Println("-> failed to get responder public key")
+		os.Exit(1)
+	}
+	fmt.Println("-> using responder public key:", responderKey.String())
+
+	conn, err := connectToResponder()
+	if err != nil {
+		fmt.Println("-> failed to connect to responder")
+		os.Exit(1)
+	}
+
+	defer func() {
+		fmt.Println("-> closing connection to the responder")
+		if err := conn.Close(); err != nil {
+			fmt.Println("-> failed to close connection to the responder")
+		}
+	}()
+
+	fmt.Println("-> connected to the responder")
 	fmt.Println("-> sending pre transaction request")
 	fmt.Println("-> received positive pre transaction response")
 	fmt.Println("-> sending transaction request")
 	fmt.Println("-> received positive transaction response")
+}
+
+func connectToResponder() (*transport.Conn, error) {
+	u := url.URL{Scheme: "ws", Host: responderAddress, Path: "/"}
+	conn, _, err := websocket.DefaultDialer.Dial(u.String(), nil)
+	if err != nil {
+		return nil, err
+	}
+	return &transport.Conn{Conn: conn}, err
 }
--- protocol/cmd/responder/main.go
+++ protocol/cmd/responder/main.go
@@ -1,7 +1,50 @@
 package main
 
-import "github.com/odysseyhack/planet-society/protocol/utils"
+import (
+	"io/ioutil"
+	"os"
+
+	"github.com/odysseyhack/planet-society/protocol/cryptography"
+	"github.com/odysseyhack/planet-society/protocol/utils"
+)
+import log "github.com/sirupsen/logrus"
 
 func main() {
 	utils.ConfigureLogger()
+	log.Infoln("responder application")
+
+	log.Infoln("creating temporary directory")
+	dir, err := ioutil.TempDir("", "responder")
+	if err != nil {
+		log.Fatalln("failed to create temporary dir:", err)
+	}
+	log.Infoln("using temporary directory:", dir)
+
+	defer func() {
+		log.Infoln("removing temporary directory:", dir)
+		if err := os.RemoveAll(dir); err != nil {
+			log.Warningln("failed to clean temporary directory:", err)
+		}
+	}()
+
+	log.Infoln("generating one shot keychain")
+	keychain, err := cryptography.OneShotKeychain()
+	if err != nil {
+		log.Fatalln("failed to generate keychain:", err)
+	}
+	log.Infoln("keychain main public key:", keychain.MainPublicKey.String())
+	log.Infoln("keychain signature key:  ", keychain.SignaturePublicKey.String())
+
+	if err := utils.WriteKeyToDir(keychain.MainPublicKey); err != nil {
+		log.Fatalln("failed to write public key:", err)
+	}
+
+	defer func() {
+		log.Infoln("removing public key")
+		if err := utils.CleanKey(); err != nil {
+			log.Warningln("cleaning key failed:", err)
+		}
+	}()
+
+	select {}
 }
