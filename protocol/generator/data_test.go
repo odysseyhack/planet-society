8d7ad937e4ef9f08fd18d303ac4e2af425217d03
phob0s <git@phob0s.pl>
2019-04-14 01:41:54 +0200

    names test

--- protocol/generator/data_test.go
+++ protocol/generator/data_test.go
@@ -2,6 +2,10 @@ package generator
 
 import "testing"
 
-func TestNoting(t *testing.T) {
-
+func TestNames(t *testing.T) {
+	for i := range names {
+		if names[i] == "" {
+			t.Errorf("name should not be empty")
+		}
+	}
 }
