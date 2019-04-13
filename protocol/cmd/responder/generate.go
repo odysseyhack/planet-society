66dd47af1ea5c55ca621af9b460a7ee5371db0f5
phob0s <git@phob0s.pl>
2019-04-13 22:43:00 +0200

    generator refactoring

--- protocol/cmd/responder/generate.go
+++ protocol/cmd/responder/generate.go
@@ -93,15 +93,7 @@ func (g *generator) generateContacts(db *database.Database) error {
 			surname := g.randomSurname()
 			display := fmt.Sprintf("%s.%s", name, surname)
 			country := g.randomCountry()
-			newContact := models.ContactInput{
-				Identity:    g.identities[i].ID,
-				PublicKey:   models.Key32{Key: cryptography.RandomKey32()},
-				Name:        &name,
-				DisplayName: display,
-				Country:     &country,
-				Surname:     &surname,
-			}
-
+			newContact := models.ContactInput{Identity: g.identities[i].ID, PublicKey: models.Key32{Key: cryptography.RandomKey32()}, Name: &name, DisplayName: display, Country: &country, Surname: &surname}
 			if _, err := db.ContactAdd(newContact); err != nil {
 				return err
 			}
@@ -114,27 +106,17 @@ func (g *generator) generateContacts(db *database.Database) error {
 func (g *generator) generateAddresses(db *database.Database) error {
 	for i := range g.identities {
 		numberOfAddresses := (g.rand.Uint32() % 2) + 1
-
 		for x := uint32(0); x < numberOfAddresses; x++ {
 			country := g.randomCountry()
 			city := g.randomCity()
 			street := g.randomStreet()
 			display := fmt.Sprintf("%s.%s", country, city)
-
-			newAddress := models.AddressInput{
-				Identity:    g.identities[i].ID,
-				City:        &city,
-				DisplayName: display,
-				Country:     &country,
-				Street:      &street,
-			}
-
+			newAddress := models.AddressInput{Identity: g.identities[i].ID, City: &city, DisplayName: display, Country: &country, Street: &street}
 			if _, err := db.AddressAdd(newAddress); err != nil {
 				return err
 			}
 		}
 	}
-
 	return nil
 }
 
@@ -142,18 +124,7 @@ func (g *generator) generatePaymentCard(db *database.Database) error {
 	for i := range g.identities {
 		name := g.randomName()
 		surname := g.randomSurname()
-
-		paymentCard := models.PaymentCardInput{
-			Identity:     g.identities[i].ID,
-			Name:         name,
-			Surname:      surname,
-			Number:       fmt.Sprintf("%d", g.rand.Uint32()),
-			DisplayName:  fmt.Sprintf("my_card"),
-			Expiration:   time.Now().Format(time.RFC3339),
-			SecurityCode: fmt.Sprintf("%d", g.rand.Uint32()%1000),
-			Currency:     g.randomCurrency(),
-		}
-
+		paymentCard := models.PaymentCardInput{Identity: g.identities[i].ID, Name: name, Surname: surname, Number: fmt.Sprintf("%d", g.rand.Uint32()), DisplayName: fmt.Sprintf("my_card"), Expiration: time.Now().Format(time.RFC3339), SecurityCode: fmt.Sprintf("%d", g.rand.Uint32()%1000), Currency: g.randomCurrency()}
 		if _, err := db.PaymentCardAdd(paymentCard); err != nil {
 			return err
 		}
@@ -165,17 +136,7 @@ func (g *generator) generatePassport(db *database.Database) error {
 	for i := range g.identities {
 		name := g.randomName()
 		surname := g.randomSurname()
-
-		passport := models.PassportInput{
-			Identity:    g.identities[i].ID,
-			Name:        name,
-			Surname:     surname,
-			Number:      fmt.Sprintf("%d", g.rand.Uint32()),
-			Expiration:  time.Now().Format(time.RFC3339),
-			DisplayName: "my_passport",
-			Country:     g.randomCountry(),
-		}
-
+		passport := models.PassportInput{Identity: g.identities[i].ID, Name: name, Surname: surname, Number: fmt.Sprintf("%d", g.rand.Uint32()), Expiration: time.Now().Format(time.RFC3339), DisplayName: "my_passport", Country: g.randomCountry()}
 		if _, err := db.PassportAdd(passport); err != nil {
 			return err
 		}
@@ -188,15 +149,7 @@ func (g *generator) generateIdentityDocument(db *database.Database) error {
 		name := g.randomName()
 		surname := g.randomSurname()
 
-		identityCard := models.IdentityDocumentInput{
-			Identity:    g.identities[i].ID,
-			Name:        name,
-			Surname:     surname,
-			Number:      fmt.Sprintf("%d", g.rand.Uint32()),
-			Country:     g.randomCountry(),
-			DisplayName: "my_id_document",
-			Expiration:  time.Now().Format(time.RFC3339),
-		}
+		identityCard := models.IdentityDocumentInput{Identity: g.identities[i].ID, Name: name, Surname: surname, Number: fmt.Sprintf("%d", g.rand.Uint32()), Country: g.randomCountry(), DisplayName: "my_id_document", Expiration: time.Now().Format(time.RFC3339)}
 		if _, err := db.IdentityDocumentAdd(identityCard); err != nil {
 			return err
 		}
