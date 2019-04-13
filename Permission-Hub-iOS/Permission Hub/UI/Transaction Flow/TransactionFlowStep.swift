f45ea2865f7e1e3f213cc8a575c1306b7c20286e
Corné Driesprong <cdriesprong@gmail.com>
2019-04-13 20:36:07 +0200

    created TransactionFlowStep

---
+++ Permission-Hub-iOS/Permission Hub/UI/Transaction Flow/TransactionFlowStep.swift
@@ -0,0 +1,43 @@
+//
+//  TransactionFlowStep.swift
+//  Permission Hub
+//
+//  Created by Corné on 13/04/2019.
+//  Copyright © 2019 Planet. All rights reserved.
+//
+
+import UIKit
+
+enum TransactionFlowStep: CaseIterable {
+    case overview, personalDetails, identityDocuments, paymentMethod, bankVerification
+
+    var title: String? {
+        switch self {
+        case .overview:
+            return nil
+        case .personalDetails:
+            return "Personal details"
+        case .identityDocuments:
+            return "Identity documents"
+        case .paymentMethod:
+            return "Payment method"
+        case .bankVerification:
+            return "Bank verification"
+        }
+    }
+
+    var viewController: UIViewController {
+        switch self {
+        case .overview:
+            return TransactionOverViewViewController()
+        case .personalDetails:
+            return PHTableViewController(title: title, items: [PHTableViewViewCellType]())
+        case .identityDocuments:
+            return PHTableViewController(title: title, items: [PHTableViewViewCellType]())
+        case .paymentMethod:
+            return PHTableViewController(title: title, items: [PHTableViewViewCellType]())
+        case .bankVerification:
+            return PHTableViewController(title: title, items: [PHTableViewViewCellType]())
+        }
+    }
+}
